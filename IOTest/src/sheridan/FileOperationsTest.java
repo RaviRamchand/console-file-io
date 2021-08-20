8731cb8e39424723ebeae877b0c163ab0ccb91e9
Ravi <raviramchand8@gmail.com>
2021-08-20 19:08:51 -0400

    call the readFile method

--- IOTest/src/sheridan/FileOperationsTest.java
+++ IOTest/src/sheridan/FileOperationsTest.java
@@ -2,8 +2,13 @@ package sheridan;
 
 public class FileOperationsTest {
 	public static void main(String []args) {
+		//Create a FileOperations object
 		FileOperations fo = new FileOperations();
 		
+		//Call the writeFile method
 		fo.writeFile();
+		
+		//Call the readFile method
+		fo.readFile();
 	}
 }
